7800aefa7df0cff0feae9dbb197ff69237603e64
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-11-15 11:08:13 +0500

    commit

--- src/app/services/vocabulary.service.ts
+++ src/app/services/vocabulary.service.ts
@@ -3,28 +3,30 @@ import { Observable } from 'rxjs';
 import { IVocabulary } from '../models/models';
 import { HttpClient } from '@angular/common/http';
 
-const HOST = 'https://vocabulary-eaf35-default-rtdb.firebaseio.com/vocabularies.json';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class VocabularyService {
+  private _host = '';
   constructor(
     private _http: HttpClient
   ) {
+    this._host = 'https://vocabulary-eaf35-default-rtdb.firebaseio.com/' + window.location.host.split('.')[0] + '.json';
+    console.log(this._host);
   }
 
   getVocabulariesList(): Observable<IVocabulary[]> {
-    return this._http.get<IVocabulary[]>(HOST);
+    return this._http.get<IVocabulary[]>(this._host);
   }
 
   addNewWord(payload: IVocabulary): Observable<{ success: boolean }> {
-    return this._http.post<{ success: boolean }>(HOST, payload);
+    return this._http.post<{ success: boolean }>(this._host, payload);
   }
 
   updateVocabulary(id: string, payload: IVocabulary): Observable<{ success: boolean }> {
-    return this._http.patch<{ success: boolean }>(HOST, JSON.stringify({
+    return this._http.patch<{ success: boolean }>(this._host, JSON.stringify({
       [id]: payload
     }));
   }
