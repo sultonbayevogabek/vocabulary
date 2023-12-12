3e3424c2ab1879ff4320275fccc35688214e3e81
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-12-12 18:45:01 +0500

    commit

--- src/app/services/vocabulary.service.ts
+++ src/app/services/vocabulary.service.ts
@@ -5,39 +5,28 @@ import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class VocabularyService {
-    private _host = '';
-    speech = new SpeechSynthesisUtterance();
+  private _host = '';
+  constructor(
+    private _http: HttpClient
+  ) {
+    this._host = 'https://vocabulary-eaf35-default-rtdb.firebaseio.com/' + window.location.host.split('.')[0] + '.json';
+  }
 
-    constructor(
-        private _http: HttpClient
-    ) {
-        this._host = 'https://vocabulary-eaf35-default-rtdb.firebaseio.com/' + window.location.host.split('.')[0] + '.json';
-        this.speech.lang = 'en-US';
-        this.speech.volume = 1;
-        this.speech.rate = 1;
-        this.speech.pitch = 1;
-    }
+  getVocabulariesList(): Observable<IVocabulary[]> {
+    return this._http.get<IVocabulary[]>(this._host);
+  }
 
-    getVocabulariesList(): Observable<IVocabulary[]> {
-        return this._http.get<IVocabulary[]>(this._host);
-    }
+  addNewWord(payload: IVocabulary): Observable<{ success: boolean }> {
+    return this._http.post<{ success: boolean }>(this._host, payload);
+  }
 
-    addNewWord(payload: IVocabulary): Observable<{ success: boolean }> {
-        return this._http.post<{ success: boolean }>(this._host, payload);
-    }
-
-    updateVocabulary(payload: IVocabulary): Observable<{ success: boolean }> {
-        return this._http.patch<{ success: boolean }>(this._host, JSON.stringify({
-            [payload.id!]: payload
-        }));
-    }
-
-    textToSpeech(word: any): void {
-        this.speech.text = word;
-        window.speechSynthesis.speak(this.speech);
-    }
+  updateVocabulary(id: string, payload: IVocabulary): Observable<{ success: boolean }> {
+    return this._http.patch<{ success: boolean }>(this._host, JSON.stringify({
+      [id]: payload
+    }));
+  }
 }
